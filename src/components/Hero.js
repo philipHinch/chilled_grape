77aa6a5a51a578af0a7bd716b85ff456371556ab
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-06-06 12:18:02 +0100

    created hero component and added blob svgs into assests folder
    hero component created and inserted under the header component. It contains the background image of the faded blobs and the 2 colored blobs

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+//blobs
+import blob1 from '../assets/images/hero_blob_left.svg'
+import blob2 from '../assets/images/hero_blob_right.svg'
+
+const Hero = () => {
+    return (
+        <section className="container heroContainer">
+            <img src={blob1} alt="blob svg" className="blob blob1" />
+            <img src={blob2} alt="blob svg" className="blob blob2" />
+            <div className="mainHeroTextContainer">
+                <h1 className="mainHeroText">Lorem ipsum dolor sit amet.</h1>
+                <button className="btn btnRounded readMoreBtn">Read More</button>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
