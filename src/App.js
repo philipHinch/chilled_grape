eabbb14576d4e71f574092a1044b878358a054e3
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-06-06 13:11:13 +0100

    created article component and added article related images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 //components
 import Hero from './components/Hero';
 import Header from './components/header/Header';
+import Article from './components/Article';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Article />
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+//images
+import plant from '../assets/images/plant.png'
+import articleBlob from '../assets/images/article_blob.svg'
+
+
+const Article = () => {
+    return (
+        <article className="articleContainer">
+            <div className="articleLeft">
+                <h2 className='articleMainText'>Lorem ipsum dolor sit amet.</h2>
+                <p className='articleParagraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div className="articleRight">
+                <img src={articleBlob} alt="" className='articleBlob' />
+                <img src={plant} alt="" className='plantImage' />
+            </div>
+        </article>
+    );
+}
+
+export default Article;
